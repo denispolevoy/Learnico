20da5fffff6c5cb4301eccb6e67cf9fa2e8b7cc0
Denis Polevoy <denispolevoy88@gmail.com>
2024-12-25 20:07:46 +0200

    The first section adapted

--- main.js
+++ main.js
@@ -9,29 +9,3 @@ const video = document.querySelector("#content__videos");
 btnClickVideo.addEventListener("click", function () {
   video.play();
 });
-
-// const generateNumbers = {
-//   start: 1,
-//   end: 10,
-// };
-
-// generateNumbers[Symbol.iterator] = function () {
-//   let start = this.start;
-//   let end = this.end;
-//   return {
-//     next() {
-//       if (start <= end) {
-//         return {
-//           done: false,
-//           value: start++,
-//         };
-//       } else {
-//         return { done: true };
-//       }
-//     },
-//   };
-// };
-
-// for (let value of generateNumbers) {
-//   console.log(value);
-// }
